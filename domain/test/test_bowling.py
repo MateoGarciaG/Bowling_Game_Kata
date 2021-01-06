5a203fa53c87f361fb276c9a040b1fe77f6bc538
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-01-06 22:42:56 +0100

    test: :white_check_mark: Se han agregado más casos test a: test_random_score()

--- domain/test/test_bowling.py
+++ domain/test/test_bowling.py
@@ -28,3 +28,8 @@ def test_heartbreak():
 def test_random_score():
     
     assert Bowling("12345123451234512345").total_score() == 60
+    assert Bowling("81-92/X637-52X-62/X").total_score() == 122
+    assert Bowling("2345XX-98/-/187251").total_score() == 107
+    assert Bowling("18X7-2/819-XXX6/3").total_score() == 158
+    assert Bowling("X63--9/5281XX1/X8/").total_score() == 140
+    assert Bowling("-2XXX16427/634/XX6").total_score() == 154
